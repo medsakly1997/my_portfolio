d16e4afa32a7d745761848f2d053ef2a31171b92
sakly mohamed khaled <saklymohamedkhaled@gmail.com>
2024-08-13 10:47:47 +0100

    feat(sakly): Initialize project and configure Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center text-blue-500">
+      <h1 className="text-3xl font-bold bg-red-500">
+        Hello, Vite + React + TypeScript + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
